3e5a68bd848814c312198aca8a6473cb12ebfbb5
Nicholas Jackson <smartguy1196@gmail.com>
2016-08-14 07:05:33 -0700

    Added room for future compatibility ideas

--- regex.objectified.js
+++ regex.objectified.js
@@ -6,18 +6,36 @@ function RegExCapture( RegExArg , string ) {
 }
 
 function RegExObj( RegExArg ){
-  this.verify = function( RegExArg ) {
-  }
-  this.convert = function( RegExArg ){
-  }
-  this.source = {
-    "regex" : {
-      "string" : RegExObj.convert( RegExArg, "string" );
-      "json" : RegExObj.convert( RegExArg, "json" );
+  if( RegExArg instanceof RegExObj ){
+    this = new RegExObj();
+    this.source = JSON.parse( JSON.stringify( RegExObj.source ) );
+    this.length = RegExObj.length;
+    for( var i = 0; i < result.length; i++) this[i] = JSON.parse( JSON.stringify( RegExObj[i] ) );
+  }else if( RegExObj.verify( RegExArg ) ){
+    this.source = {
+      "regex" : {
+        "string" : RegExObj.convert( RegExArg, "string" );
+        "json" : RegExObj.convert( RegExArg, "json" );
+      }
     }
+    this.length = 0;
+    this[0] = null;
+    
+    //Construct this[i]...
+    
+  }else{
+    this.verify = function( RegExArg ) {
+      
+    }
+    this.convert = function( RegExArg, option ){
+      
+    }
+    this.length = 0;
+    this[0] = null;
   }
-  this[0] = null;
-  
-  //Construct this[i]...
-  
+}
+
+RegExObj.prototype = {
+  "verify" : null,
+  "convert" : null
 }
