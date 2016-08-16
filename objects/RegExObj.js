2d6299afcf4581bdb45a8d66a4ef1c1efe579c84
Nicholas Jackson <smartguy1196@gmail.com>
2016-08-15 23:28:41 -0700

    add string to array function
    originally the idea was to check for groups first. In next commit, escapes will be found first

--- objects/RegExObj.js
+++ objects/RegExObj.js
@@ -3,17 +3,42 @@ function RegExObj( RegExArg ){
   //used for detecting if RegExArg exists and that it is a valid parameter
   
   var NaR = false;
-  var regex_validator = function( regex, bool ){
-  var result = [],
-      i = 0,
-  var tree_height = 0;
-  result[i] = {
-    "regex" : regex,
-    "type" : "string"
+  function StringToArray( RegExArg ){
+    var result_array = [
+        {
+          "regex" : RegExArg,
+          "token_type" : "string"
+        }
+      ],
+      debug = {
+        "tree_height" : 0;
+      },
+      handler = {
+        "match",
+        "regex" = /^(\(#\[)|^((?:\(\?(?:!|:!?|=|<(?:=|!)?|)))|^(\()|^(\))/g
+      },
+      loop = {
+        "r_i" : 0, //result_arrray index
+        "pm_i" : 0 //previous match index
+      }
+    while( ( handler.match = handler.regex.exec( RegExArg ) ) != null ){
+      if( RegExArg.substring( match.index + match[ 0 ].length, 0 ).length > 0 ){
+        result[ loop.r_i+1 ] = {
+          "regex" : RegExArg.substring( match.index + match[ 0 ].length, 0 ),
+          "token_type" : "string"
+        }
+      }
+      if( match[ 4 ] === ""){
+        if( match[ 1 ] != ""){
+          result[ loop.r_i ].type = "capture_start"
+        }
+      }else{
+        result[ loop.r_i ].type = "capture_end"
+        debug.tree_height--;
+      }
+    }
   }
-  var re = /^((?:\((?:\?(?:!|:!?|=|<(?:=|!)?|))?))|^(\))/g,
-      match,
-      p = 0;
+  var regex_validator = function( regex, bool ){
   while ((match = re.exec(regex)) != null) {
     if(regex.substring(match.index,0).length > 0){}
       result[i+1].regex = regex.substring(match.index,0);
@@ -26,7 +51,7 @@ function RegExObj( RegExArg ){
       }
     }
     if(regex.substring(p,match.index).length > 0){
-      result[i].regex = regex.substring(p,match.index);
+      result[i].regex = match[0];
     }
     p = match.index;
     i++;
