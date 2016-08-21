f0a7d9e987313bacc99b1771ff42e131493f3589
Nicholas Jackson <smartguy1196@gmail.com>
2016-08-21 14:07:30 -0700

    Revise JSONifier.js, Rename to Objectifier.js, Work on Issue #6
    https://github.com/smartguy1196/regex.objectified/issues/6 - relevant issue
    
    Fixed build to allow extension functionality
    Added new comments for explanatory purposes
    Built stringToObject function to better fit inside a constructor function or serve as a constructor function itself...
    
    Current revision is still under draft

--- libraries/Objectifier.js
+++ libraries/Objectifier.js
@@ -1,63 +1,144 @@
-function stringToJSON( RegExArg ){
-  if( /^\//.test( RegExArg ) ){
+//Directly convert String to Object
+
+function stringToObj( RegExArg ){
+  
+  //Argument must start with "/", but we don't want it as it isn't useful
+  
+  if( /^\//.test( RegExArg ) )
+  {
     RegExArg = RegExArg.substring( 1 );
-  }else{
+  }
+  else
+  {
     
     //Error, RegExArg is not a valid regex-string
     
   }
-  if( ( match = /\/([A-Za-z]*)$/.exec( RegExArg ) ) ){
-    var flags = match[ 1 ];
-    RegExArg = RegExArg.substring( 0, ( RegExArg.length -2 -match[ 1 ].length ) );
-  }else{
+  
+  //Argument must end with a "/" or a "/" followed by a flag, however this information is not useful for creating the object
+  //Flags will be stored to the RegExObj
+  //The inner value of the original argument is now the argument
+  //This value is also all characters with indexes after the initial "/" up to the one where it equals the length of RegExArg without the initial "/" minus 1 (length to last index conversion) minus 1 (last "/") minus the length of the flag
+  
+  if( ( match = /\/([A-Za-z])/ ) ){
+    this.flags = match[ 1 ];
+    RegExArg = RegExArg.substring( 0, ( RegExArg.length -2 -match[1].length ) )
+  }
+  else
+  {
     
     //Error, RegExArg is not a valid regex-string
     
   }
-  var result_JSON = [
+  
+  //initialize the RegExObj's Tokens
+  
+  this.tokens = [
       {
         "regex" : RegExArg,
-        "token_type" : "string"
+        "type" : "string"
       }
-    ],
-    debug = {
-      "tree_height" : 0,
-      "string_tracker" : [ 0 ], //Used for tracking unknown strings
-      "mini_s_tracker" : [ 0 ]
+    ];
+  var debug = {
+      "group_depth" : 0, //Used for tracking nested groups and classes
+      "string_tracker" : { //Used for tracking UNIDENTIFIED strings. Main and Mini alternate between indeces 0 and 1 respectively
+        "first" : [ 0 ], 
+        "second" : [ 0 ]
+      }
     },
     loop = {
-      "r_i" : 0, //result_JSON index
-      "st_i" : 0, //string_tracker index
-      "mr_i" : 0 //mini_result index
+      "t_i" : { //token index. Main and Mini alternate between indeces 0 and 1 respectively
+        "first" : 0,
+        "second" : 0
+      },
+      "st_i" : { //string_tracker index. Main and Mini alternate between indeces 0 and 1 respectively
+        "first" : 0,
+        "second" : 0
+      },
+      "r_i" : 0 //used for creating regex strings for searching
     };
   
-  //Does the RegExArg inner value have escape sequences ("\")?
+  //Does the RegExArg inner (current) value have escape sequences ("\")?
   
-  if( ( match = /\\/.exec( result_JSON[ loop.r_i ].regex ) ) != null ){
-    result_JSON[ loop.r_i+1 ].regex = result_JSON[ loop.r_i ].regex.substring( match.index );
-    result_JSON[ loop.r_i+1 ].token_type = "string";
-    result_JSON[ loop.r_i ].regex = result_JSON[ loop.r_i ].regex.substring( 0, match.index );
-    result_JSON[ loop.r_i ].token_type = "string";
-    loop.r_i++;
-    for( ; result_JSON.length > loop.r_i && ( match = /^\\/.exec( result_JSON[ loop.r_i ].regex ) ) != null; loop.r_i++){
+  if( ( match = /\\/.exec( this.tokens[ loop.t_i.first ].regex ) ) != null )
+  {
+    
+    //Create new element in tokens and assign it to the substring that starts with the first instance of "\"
+    //This one will be used for creating the escape token
+    
+    this.tokens[ loop.t_i.first+1 ].regex = this.tokens[ loop.t_i.first ].regex.substring( match.index );
+    this.tokens[ loop.t_i.first+1 ].type = "string"; //Debugger will not be tracking this string YET
+    
+    //Create token for the unknown string before the escape character
+    //Debugger does not need to add a tracker for this one as it is already tracking the string at index 0
+    
+    this.tokens[ loop.t_i.first ].regex = this.tokens[ loop.t_i.first ].regex.substring( 0, match.index );
+    this.tokens[ loop.t_i.first ].type = "string";
+    
+    //starting by increasing loop.t_i.first by 1, loop through matches (escapes) found in the unknown strings
+    //the loop only continues if the token index is less than the entire tokens length AND if a match exists
+    
+    for( loop.t_i.first++; this.tokens.length > loop.t_i.first && ( match = /^\\/.exec( this.tokens[ loop.t_i.first ].regex ) ) != null; loop.t_i.first++ )
+    {
       
       //Does the string following the matching "\" have a length greater than 0?
       
-      if( result_JSON[ loop.r_i ].regex.substring( match[ 0 ].length ).length > 0 ){
+      if( this.tokens[ loop.t_i.first ].regex.substring( match[ 0 ].length ).length > 0 )
+      {
         
-        //Are the first few characters in the following string a SPECIAL escape sequence?
+        //Are the first few characters in the following string a SPECIAL escape sequence? (prep)
+        //Using these escape regexes:
         
-        if( ( match2 = /^(\d\d\d?)|^(x[A-Fa-f0-9][A-Fa-f0-9])|^(u[A-Fa-f0-9][A-Fa-f0-9][A-Fa-f0-9][A-Fa-f0-9])|^(c[A-Za-z])|^([1-9])/.exec( result_JSON[ loop.r_i ].regex.substring( match[ 0 ].length ) ) ) != null ){
+        var escapes = [
+            "^(\\d\\d\\d?)", //Octal
+            "^(x[A-Fa-f0-9]{2})", //Hexadecimal
+            "^(u[A-Fa-f0-9]{4})", //Unicode
+            "^(c[A-Za-z])", //Control Character
+            "^([1-9])" //Back Reference
+          ],
+          regex_string; //This is used for creating the regex that finds the escapes
+        
+        //Create regex_string
+        
+        for( loop.r_i = 0; escapes.length > loop.r_i; loop.r_i++ )
+          regex_string = regex_string + ( loop.r_i === 0 ? "/" : "|" ) + escapes[ loop.r_i ];
+        regex_string = regex_string + "/";
+        
+        //Are the first few characters in the following string a SPECIAL escape sequence? (check)
+        
+        if( ( match2 = new RegExp( regex_string ).exec( this.tokens[ loop.t_i.first ].regex.substring( match[ 0 ].length ) ) ) != null)
+        {
           
           //Is the SPECIAL escape sequence an octal escape?
           
           if( match2[ 1 ].length != 0 ){
-            result_JSON[ loop.r_i+1 ].regex = result_JSON[ loop.r_i ].regex.substring( match2[ 1 ].length + 1 );
-            result_JSON[ loop.r_i ].regex = '\\' + match2[ 1 ];
-            result_JSON[ loop.r_i ].token_type = "escape::octal::len" + match2[ 1 ].length;
+            this.tokens[ loop.t_i.first+1 ].regex = this.tokens[ loop.t_i.first ].regex.substring( match2[ 1 ].length );
+            this.tokens[ loop.t_i.first ].regex = '\\' + match2[ 1 ];
+            this.tokens[ loop.t_i.first ].type = "escape::octan::len" + match2[ 1 ].length;
           }
+        }
+      }
+      
+      else //In the case that there is no string following "\"
+      
+      {
+        
+      }
+    }
+  }
+}
           
           //Is the SPECIAL escape sequence a hexadecimal escape?
+          
+          //remember to change VERBAGES!!!
+          //remember to change VERBAGES!!!
+          //remember to change VERBAGES!!!
+          //remember to change VERBAGES!!!
+          //remember to change VERBAGES!!!
+          //remember to change VERBAGES!!!
+          //remember to change VERBAGES!!!
+          //remember to change VERBAGES!!!
+          //remember to change VERBAGES!!!
           
           if( match2[ 2 ].length != 0 ){
             result_JSON[ loop.r_i+1 ].regex = result_JSON[ loop.r_i ].regex.substring( 4 );
