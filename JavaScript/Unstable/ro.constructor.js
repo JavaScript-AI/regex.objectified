66d3568336f24b672c880cbb5ee27072a31dbc38
smartguy1196 <smartguy1196@gmail.com>
2016-09-20 18:35:48 -0700

    Continue on Draft 3

--- JavaScript/Unstable/ro.constructor.js
+++ JavaScript/Unstable/ro.constructor.js
@@ -100,7 +100,7 @@ function RegExSearch( RegExArg )
 		//if the ending characters in RegExArg are a "/" or a "/" followed by a flag, attach the flags to the RegExSearch Object as a property ("this.flag") and cut this ending off as it is not needed,
 		//else log the above error and return "false" signifying a failed construction
 
-		if( ( match = ( new RegExp( "/\\/([A-Za-z])/" ) ).exec( RegExArg ) ) ) //if the ending characters in RegExArg are a "/" or a "/" followed by a flag,
+		if( ( native_match = ( new RegExp( "/\\/([A-Za-z])/" ) ).exec( RegExArg ) ) ) //if the ending characters in RegExArg are a "/" or a "/" followed by a flag,
 		{
 
 			//attach the flags to the RegExSearch Object as a property ("this.flag")
@@ -165,14 +165,14 @@ function RegExSearch( RegExArg )
 		},
 		"detector_sync" : { //Used for storing data over different Detector Loops
 
-			"prev_string_tracker_index": -1 //Used to help detect whether the current value existing at the currently-being checked string tracker index has changed, if it did not, detector will stop looping, so that the next token can be cycled through
+			"prev_string_tracker_value": -1 //Used to help detect whether the current value existing at the currently-being checked string tracker index has changed, if it did not, detector will stop looping, so that the next token can be cycled through
 
 		},
 		"incremental_sync" : {
 
-			"string_tracker_index" : 0, //Incremental value also referred to as the current string tracker index that is used to help access the current string tracker value
+			"string_tracker_id_num" : 0, //Incremental value also referred to as the current string tracker index that is used to help access the current string tracker value
 			"native_regex_part_index" : 0, //Incremental value used to assemple native regex parts into a complete native regex object
-			"string_tracker_index_not_current" : 0 //Incremental value used to help apply changes to string tracker values that ar not the current string tracker value
+			"string_tracker_id_num_not_current" : 0 //Incremental value used to help apply changes to string tracker values that ar not the current string tracker value
 
 		}
 
@@ -190,19 +190,19 @@ function RegExSearch( RegExArg )
 
 		**************************************************************************************************************************************************/
 
-	for( //TOKEN CYCLER (loops through unidentified string tokens: tokens that could possibly have undiscovered (identifiable) tokens within them)
+	for( //Token Cycler (loops through unidentified string tokens: tokens that could possibly have undiscovered (identifiable) tokens within them)
 
 		//reinitialize the string tracker index to 0
 
-		debug.incremental_sync.string_tracker_index = 0;
+		debug.incremental_sync.string_tracker_id_num = 0;
 
 		//if the amount of checked strings plus 1 (amount is based off of the current string tracker index - already checked values are everything, but the value found at the current string tracker index, because that value has not yet been checked) (if the currently checking index) is less than the total amount of tracked strings, loop through checks
 
-		debug.incremental_sync.string_tracker_index < debug.string_tracker.length;
+		debug.incremental_sync.string_tracker_id_num < debug.string_tracker.length;
 
 		//after every loop increment the checking index by 1
 
-		debug.incremental_sync.string_tracker_index++;
+		debug.incremental_sync.string_tracker_id_num++;
 
 	){
 
@@ -210,7 +210,206 @@ function RegExSearch( RegExArg )
 
 	    debug.detector_sync = {
 
-	    	"prev_string_tracker_index": -1
+	    	"prev_string_tracker_value": -1
     	
 	    }
+
+		/**************************************************************************************************************************************************
+
+		DRAFT 3 (PART IV):
+
+		- Token Detectors: Escape (Non-Groupable: Sequential)
+
+		**************************************************************************************************************************************************/
+
+		while( //Escape Detector
+
+			//Matcher Clause
+
+			(
+				native_match = ( 
+					new RegExp( "/\\\\/" )
+				).exec(
+					this.tokens[
+
+						debug.string_tracker[
+
+							debug.incremental_sync.string_tracker_id_num
+
+						]
+
+					].regex
+				)
+			) != null &&
+
+			//Switcher Clause
+
+			(
+				debug.cycler_sync.detector = (
+					debug.cycler_sync.detector === 0
+
+					? (
+						debug.detector_sync.prev_string_tracker_value 
+						
+						<
+						
+						debug.string_tracker[
+
+							debug.incremental_sync.string_tracker_id_num
+
+						]
+
+						? 0 : 1
+					) : debug.cycler_sync.detector
+				)
+			) === 0
+		){
+
+			//if match the match is NOT at the beginning (match.index !== 0) of the string, make the first token be all the junk before the "\", and create a new token that will be a Mixed Token (Escape/Unidentified)
+
+			if( native_match.index !== 0 ) //Preceding Unidentified String Detector
+			{
+
+				//First, create the Mixed Token (handle match, so that we can discard the original this.tokens[#].regex)
+
+				//Set the Mixed Token's regex
+
+				this.tokens[
+
+					1 + debug.string_tracker[
+
+						debug.incremental_sync.string_tracker_id_num
+
+					]
+
+				].regex = this.tokens[
+
+					debug.string_tracker[
+
+						debug.incremental_sync.string_tracker_id_num
+
+					]
+
+				].regex.substring( native_match.index )
+
+				//Set the Mixed Token's type to be a mixed string (string::mixed)
+
+        		this.tokens[
+
+					1 + debug.string_tracker[
+
+						debug.incremental_sync.string_tracker_id_num
+
+					]
+
+				].type = "string::mixed";
+
+				//Secondly, create the Unidentified String that exists before the escape character ("\"), as it has already been processed (it is already being tracked)
+
+				//Set the Preceding Unidentified Token's regex
+
+				this.tokens[
+
+					debug.string_tracker[
+
+						debug.incremental_sync.string_tracker_id_num
+
+					]
+
+				].regex = this.tokens[
+
+					debug.string_tracker[
+
+						debug.incremental_sync.string_tracker_id_num
+
+					]
+
+				].regex.substring( 0, native_match.index );
+
+				//Set the Preceding Unidentified Token's type to be an unidentified string (string::unidentified)
+
+        		this.tokens[
+
+					debug.string_tracker[
+
+						debug.incremental_sync.string_tracker_id_num
+
+					]
+
+				].type = "string::unidentified";
+
+				//Since we inserted a new unidentified string for the Preceding Unidentified String, but created a new token for the Mixed Token,
+				//we will just keep the string tracker value at the current string tracker ID the same, and add a new tracker
+
+					//DRAFT 3: According to Draft 3 of Regex.Objectified, Sequential Detectors (Escape Detectors) do not require and therefore will not have Tracker Incrementsrs, just Additions
+
+				//Tracker Addition:
+
+				debug.string_tracker[
+
+					1 + debug.incremental_sync.string_tracker_id_num
+
+				] = 1 + debug.string_tracker[
+
+					debug.incremental_sync.string_tracker_id_num
+
+				]
+				debug.incremental_sync.string_tracker_id_num++;
+
+			} //Preceding Unidentified String Detector
+			else
+			{ //There was no Preceding Unidentified String
+
+				//Since there was no Preceding Unidentified String, that means that the Mixed token starts at the beginning,
+				//or otherwise meaning that the token at the string tracker value is not an Unidentified String Token, but a Mixed Token,
+				//so we only change its type from Unidentified to Mixed
+
+				//assign the currently existing token, which DOES NOT require change in the regex property, to the Mixed Token type
+
+				this.tokens[
+
+					debug.string_tracker[
+
+						debug.incremental_sync.string_tracker_id_num
+
+					]
+
+				].type = "string::mixed";
+
+			} //Preceding Unidentified String Detector (FAILED)
+
+		}
+
+		/**************************************************************************************************************************************************
+
+		DRAFT 3 (PART IV):
+
+		- Token Detectors: Class (Groupable:No-Nesting)
+
+		**************************************************************************************************************************************************/
+
+		/**************************************************************************************************************************************************
+
+		DRAFT 3 (PART IV):
+
+		- Token Detectors: Shorthand (Non-Groupable:Shorthand)
+
+		**************************************************************************************************************************************************/
+
+		/**************************************************************************************************************************************************
+
+		DRAFT 3 (PART IV):
+
+		- Token Detectors: Groups (Groupable:Nesting)
+
+		**************************************************************************************************************************************************/
+
+		/**************************************************************************************************************************************************
+
+		DRAFT 3 (PART IV):
+
+		- Token Detectors: Quantifiers/Attributes (Groupable:No-Nesting)
+
+		**************************************************************************************************************************************************/
+
 }
